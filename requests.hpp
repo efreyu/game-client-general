c806ab49a50e20458f3b70f9241e1ec922339b39
Artur Barilo <efreyu@gmail.com>
2024-04-16 22:16:23 +0300

    master | gameplay, fast prototyping /wip

--- requests.hpp
+++ requests.hpp
@@ -5,12 +5,14 @@ namespace mg::inline network::requests {
     namespace client {
         // setting server time on client
         constexpr auto set_server_time = "set_server_time";
-    }
+    }  // namespace client
 
     namespace server {
         // get or create user on server
-        constexpr auto get_or_create_user = "get_or_create_user";
-    }
+        namespace usersService {
+            constexpr auto get_or_create_user = "get_or_create_user";
+        }
+    }  // namespace server
 }  // namespace mg::inline network::requests
 
 #endif  // MGPROJECT_REQUESTS_HPP
